009f5fc5312b5926c96f3bc95cd15aef1dd8d430
Leonardo Felipe Celestino <leoncelestino@outlook.com>
2024-02-24 01:32:12 -0300

    fix: organized codde lil beat. feat: added new rows func

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,72 +1,30 @@
 const word = "wordle";
 
-const generateGrid = (word="") => {
+const generateGrid = (word="", createFirstInputs) => {
     const length = word.length;
     const wordle = document.querySelector("#Wordle");
     const frag = document.createDocumentFragment();
     for (let i = 0; i < length*4; i++) {
         const div = document.createElement("div");
-        const textField = document.createElement("input");
         div.classList.add("js-card", "card", "m-card-style");
-        textField.classList.add("text-field", "js-inputField");
-        textField.setAttribute("type", "text");
-        textField.setAttribute("placeholder", "o");
-        textField.setAttribute("maxlength", "1");
-        textField.setAttribute("value", "");
-        textField.setAttribute("data-elfocus", `${i}`);
-        div.appendChild(textField);
+
+        if (i < word.length) {
+            div.appendChild(createFirstInputs());
+        }
         frag.appendChild(div);
     }
 
     wordle.appendChild(frag);
-
-    const card = document.querySelector(".js-card");
     document.documentElement.style.setProperty("--columns", length);
 
-    const cardRect = card.getBoundingClientRect();
-    const newHeight = cardRect.width / 16;
-
-    document.documentElement.style.setProperty("--row-size", `${newHeight}rem`);
 } 
 
-generateGrid(word);
+generateGrid(word, createInputs);
 
-const inputField = document.querySelectorAll(".js-inputField");
+addEventsToInputs();
 
-inputField.forEach((input, index) => {
-    input.addEventListener("input", (e) => {
-        const target = e.target;
-        target.value = target.value.replace(/[^A-Za-z]/g, "");
-    })
-})
 
-inputField.forEach((input, index) => {
-    input.addEventListener("keydown", (e) => {
-        const target = e.target;
-        const key = e.key.length === 1 ? e.key : "";
-
-        if (e.code === "Backspace" && !target.value  && inputField[index - 1]) {
-            inputField[index - 1].value = "";
-        }
-
-        if (target.value && key) {
-            inputField[index + 1].value = key;
-        }
-
-    })
-
-    input.addEventListener("keyup", (e) => {
-        const target = e.target;
-        if (target.value.length) {
-            inputField[index + 1].focus();
-        }
-
-        if (e.code === "Backspace" && !target.value) {
-            inputField[index - 1].focus();
-        }
-
-    })
-})
+const boxes = document.querySelectorAll(".js-card");
 
 const inputs = (fields=[], startIndex) => {
     const endIndex = startIndex + word.length;
@@ -93,7 +51,11 @@ const checkIfRowIsFilled = (fields=[], startIndex) => {
 const checkLettersInCorrectPlace = (word, inputs) => {
     inputs.forEach((letter, index) => {
         if (letter.value.toLowerCase() === word[index].toLowerCase()) {
+            const p = document.createElement("p");
+            p.classList.add("letter");
+            p.textContent = letter.value;
             letter.closest("div").style.backgroundColor = "var(--right-position)";
+            letter.closest("div").replaceChildren(p);
         }
     })
 }
@@ -101,7 +63,11 @@ const checkLettersInCorrectPlace = (word, inputs) => {
 const isLetterInWord = (word, inputs) => {
     inputs.forEach((letter, index) => {
         if (word.includes(letter.value) && !(letter.value === word[index]) ) {  
+            const p = document.createElement("p");
+            p.classList.add("letter");
+            p.textContent = letter.value;
             letter.closest("div").style.backgroundColor = "var(--is-in-word)";
+            letter.closest("div").replaceChildren(p);
         }
     })
 }
@@ -109,26 +75,112 @@ const isLetterInWord = (word, inputs) => {
 const isLetterWrong = (word, inputs) => {
     inputs.forEach((letter) => {
         if (!word.includes(letter.value)) {  
+            const p = document.createElement("p");
+            p.classList.add("letter");
+            p.textContent = letter.value;
             letter.closest("div").style.backgroundColor = "var(--wrong-position)";
+            letter.closest("div").replaceChildren(p);
         }
     })
 }
 
-/* TODO:
-        Check if first row is entirely filled,
-        Compare positions of the letters with the position of "Word"
-        Handle styling according with if the letter exists in the word, if it exists and is in the correct position and if it doesn't exists
-    */
+const createNewRow = (boxes, word, index, createInputs) => {
+    for (let i = 0; i < word.length; i++) {
+        boxes[i + index*word.length].appendChild(createInputs());
+    }
+}
+
+const numberOfTries = () => {
+    let newTry = 0;
+    return function clos(update){
+        newTry += update;
+        return newTry;
+    }
+
+}
+
+const updateTries = numberOfTries();
+
+/* When user press enter */
 
 document.addEventListener("keyup", (e) => {
     const enter = "Enter";
-    const isRowFilled = checkIfRowIsFilled(inputField, 0);
-    if (e.code === enter && isRowFilled) {
-        checkLettersInCorrectPlace(word, inputs(inputField, 0));
-        isLetterInWord(word, inputs(inputField, 0));
-        isLetterWrong(word, inputs(inputField, 0));
-        console.log(isRowFilled);
+
+    if (e.code === enter) {
+        const inputField = document.querySelectorAll(".js-inputField");
+        const isRowFilled = checkIfRowIsFilled(inputField, 0);
+        const tries = updateTries(1);
+
+        if (isRowFilled && tries < 5) {
+            checkLettersInCorrectPlace(word, inputs(inputField, 0));
+            isLetterWrong(word, inputs(inputField, 0));
+            isLetterInWord(word, inputs(inputField, 0));
+            createNewRow(boxes, word, tries, createInputs);
+            addEventsToInputs();
+
+            const newInput = document.querySelector(".js-inputField");
+            newInput.focus();
+
+        }
+
     }
 })
 
 
+function addEventsToInputs() {
+    const inputField = document.querySelectorAll(".js-inputField");
+
+    inputField.forEach((input, index) => {
+        input.addEventListener("input", (e) => {
+            const target = e.target;
+            target.value = target.value.replace(/[^A-Za-z]/g, "");
+        })
+    })
+
+    inputField.forEach((input, index) => {
+        input.addEventListener("keydown", (e) => {
+            const target = e.target;
+            const key = e.key.length === 1 ? e.key : "";
+
+            if (e.code === "Backspace" && !target.value  && inputField[index - 1]) {
+                inputField[index - 1].value = "";
+            }
+
+            if (target.value && key) {
+                inputField[index + 1].value = key;
+            }
+
+        })
+
+    input.addEventListener("keyup", (e) => {
+            const target = e.target;
+            if (target.value.length) {
+                inputField[index + 1].focus();
+            }
+
+            if (e.code === "Backspace" && !target.value) {
+                inputField[index - 1].focus();
+            }
+
+        })
+    })
+
+    return inputField;
+}
+
+
+function createInputs() {
+    const attributes = {
+        type: "text",
+        placeholder: "o",
+        maxlength: "1",
+        value: "",
+    }
+    const input = document.createElement("input");
+    input.classList.add("text-field", "js-inputField");
+    for (let [key, value] of Object.entries(attributes)) {
+        input.setAttribute(key, value);
+    }
+
+    return input;
+}
